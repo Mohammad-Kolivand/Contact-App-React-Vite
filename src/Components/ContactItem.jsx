16baee7300a0fb691823724e660f578218365f39
Mohammad <mohammadreza.financial@gmail.com>
2025-05-11 12:08:05 +0330

    Uploading Project

---
+++ src/Components/ContactItem.jsx
@@ -0,0 +1,24 @@
+import styles from "./ContactItem.module.css";
+function ContactItem({
+  data: { id, name, lastname, email, phone },
+  deleteHandler,
+}) {
+  return (
+    <li className={styles.item}>
+      <p>
+        {name} {lastname}
+      </p>
+      <p>
+        <span>📩</span>
+        {email}
+      </p>
+      <p>
+        <span>📞</span>
+        {phone}
+      </p>
+      <button onClick={() => deleteHandler(id)}>🗑️</button>
+    </li>
+  );
+}
+
+export default ContactItem;
